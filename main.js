6caadf93296709b8b1d91c8eea90cc7f9b79adc2
Mauro torrisi <Maurotorrisi100@gmail.com>
2024-01-23 16:54:00 -0300

    Se agrego metodo filter

--- main.js
+++ main.js
@@ -30,3 +30,25 @@ if(productos.includes(buscador)){
 else alert("Producto no disponible")
 
 }
+
+
+// METODO FILTER
+
+const Articulos = [{id: 1 , nombre:"camisa",precio:1000},
+{id: 2 , nombre:"pantalon",precio:1500},
+{id: 3 , nombre:"media",precio:109},
+{id: 4 , nombre:"gorra",precio:1220},
+{id: 5 , nombre:"calza",precio:1100}]
+
+let Precio = Number(prompt("Cuanto dinero tienes los precios rondan entre 1000 y 1500"))
+
+const productosFiltrados = Articulos.filter(item=>item.precio <= Precio)
+
+
+productosFiltrados.forEach((item)=>{
+    alert(`id:${item.id}
+nombre:${item.nombre}
+precio:${item.precio}`
+    )
+}
+)
